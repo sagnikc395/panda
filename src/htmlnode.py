5cf0d72beacd14e0be6c7d81d45fe275e37ec28d
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-07 16:01:09 +0530

    src: added leafnode code

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,7 +20,32 @@ class HTMLNode:
     """
 
     def props_to_html(self):
-        pass
+        return ' '.join([f'{key}="{value}"' for key, value in self.props.items()])
+
 
     def __repr__(self):
-        pass
+        return f"{self.tag} {self.value} {self.children} {self.props}"
+
+class LeafNode(HTMLNode):
+    '''
+    shouldnt allow for children 
+    the value data member is required.
+    '''
+    def __init__(self,tag,value,props):
+        super().__init__(tag,value,props)
+
+    '''
+    to_html method should render a leaf node as a HTML string.
+    if the leaf node has no value , it should then raise a ValueError.
+    All leaf nodes would require a value.
+
+    If tag is None, then the value should be returned as raw text.
+    '''    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("leaf node has no value")
+        elif not self.tag:
+            return f"{self.value}"
+        else:
+            return f"<{self.tag} {super().props_to_html()}> {self.value} </{self.tag}>" 
+
