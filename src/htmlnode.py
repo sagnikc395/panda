4083f4279e4ff084811da9adbeac0427e7ad3495
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-21 02:23:14 +0530

    src: added textnode and complete htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,52 +20,58 @@ class HTMLNode:
     """
 
     def props_to_html(self):
-        return ' '.join([f'{key}="{value}"' for key, value in self.props.items()])
-
+        return " ".join([f'{key}="{value}"' for key, value in self.props.items()])
 
     def __repr__(self):
         return f"{self.tag} {self.value} {self.children} {self.props}"
 
-class LeafNode(HTMLNode):
-    '''
-    shouldnt allow for children 
-    the value data member is required.
-    '''
-    def __init__(self,tag,value,props):
-        super().__init__(tag,value,props)
 
-    '''
+class LeafNode(HTMLNode):
+    """
+    shouldnt allow for children
+    the value data member is required.
+    """
+
+    def __init__(self, tag, value, props):
+        super().__init__(tag, value, props)
+
+    """
     to_html method should render a leaf node as a HTML string.
     if the leaf node has no value , it should then raise a ValueError.
     All leaf nodes would require a value.
 
     If tag is None, then the value should be returned as raw text.
-    '''    
+    """
+
     def to_html(self):
         if not self.value:
             raise ValueError("leaf node has no value")
         elif not self.tag:
             return f"{self.value}"
         else:
-            return f"<{self.tag} {super().props_to_html()}> {self.value} </{self.tag}>" 
+            return f"<{self.tag} {super().props_to_html()}> {self.value} </{self.tag}>"
+
 
 class ParentNode(HTMLNode):
-    '''
-     handle the nesting of HTML nodes inside of one another. Any HTML node that's not 'leaf' node (ie has children) is a parent node.
-    '''
-    def __init__(self,tag,children):
-        super().__init__(tag,children)
-    
+    """
+    handle the nesting of HTML nodes inside of one another. Any HTML node that's not 'leaf' node (ie has children) is a parent node.
+    """
+
+    def __init__(self, tag, children):
+        super().__init__(tag, children)
+
     def to_html(self):
         if not self.tag:
             raise ValueError("parentnode tag not provided")
         elif not self.children:
             raise ValueError("parentnode has no children present")
         else:
-            '''
+            """
             return a string repr of the HTML tag of the node and its children. should be a recusive method (with each recursion using a new node instance). 
-            '''
-            pass 
-
-
+            """
+            html = f"<{self.tag}>"
+            for child in self.children:
+                html += child.to_html()
+            html += f"</{self.tag}>"
+            return html
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,8 @@
+from enum import Enum
+
+from htmlnode import LeafNode
+
+
 class TextNode:
     """
     text: text content of the node
@@ -23,4 +28,30 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
+
+
+class NodeTypes(Enum):
+    text_type_text = "text"
+    text_type_bold = "bold"
+    text_type_italic = "italic"
+    text_type_code = "code"
+    text_type_link = "link"
+    text_type_image = "image"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type not in NodeTypes:
+        raise Exception("invalid text node type!!!")
+    else:
+        if text_node.text_type == NodeTypes.text_type_text:
+            return LeafNode(text_node.text)
+        elif text_node.text_type == NodeTypes.text_type_bold:
+            return LeafNode("b", text_node.text)
+        elif text_node.text_type == NodeTypes.text_type_italic:
+            return LeafNode("i", text_node.text)
+        elif text_node.text_type == NodeTypes.text_type_code:
+            return LeafNode("code", text_node.text)
+        elif text_node.text_type == NodeTypes.text_type_link:
+            return LeafNode("a", text_node.text, text_node.url)
+        elif text_node.text_type == NodeTypes.text_type_image:
+            return LeafNode("img", "", (text_node.url, text_node.text))
