496cd1c26fe92386f9e4fe3a535256d779d7f311
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-21 12:19:46 +0530

    src: added split delimitor

--- src/textnode.py
+++ src/textnode.py
@@ -52,3 +52,25 @@ def text_node_to_html_node(text_node: TextNode):
         return LeafNode("a", text_node.text, text_node.url)
     if text_node.text_type == NodeTypes.text_type_image:
         return LeafNode("img", "", (text_node.url, text_node.text))
+
+def split_nodes_delimiter(old_nodes,delimiter,text_type):
+    nodes = []
+    for node in nodes:
+        if node.text_type != NodeTypes.text_type_text:
+            node.append(node)
+        
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError('Inavlid markdown, formatted sections are not closed')
+        
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue 
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i],text_type=NodeTypes.text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i],text_type))
+
+        nodes.extend(split_nodes)
+    return nodes 
